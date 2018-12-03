3ce9da4b078ace2a4df02b797d91f062067e7b02
Jay Sargent <sargentjay11@gmail.com>
2018-12-03 11:17:52 -0700

    reordering with numbers complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo1 from './assets/starwars.png'
 import logo2 from './assets/movierater.png'
+import { DragDropContext, Droppable} from 'react-beautiful-dnd'
 import './App.css';
 import Axios from 'axios';
 import Movie from './components/Movie';
@@ -10,15 +11,35 @@ class App extends Component {
     films: []
   }
   componentDidMount() {
+    console.log('mounted');
     Axios.get('https://swapi.co/api/films').then((res) => {
       this.setState({ films: res.data.results })
     })
   }
+  onDragEnd = result => {
+    const  {destination, source} = result;
+    let { films } = this.state;
+
+    if (!destination) {
+      return;
+    }
+
+    if (destination.index === source.index) {
+      return;
+    }
+
+    let tempFilm = films[source.index]
+    let newFilmArr = films.slice();
+    newFilmArr.splice(source.index, 1);
+    newFilmArr.splice(destination.index, 0, tempFilm);
+    this.setState({ films: newFilmArr });
+
+  }
   render() {
-    let films = this.state.films.map(item => {
+    let films = this.state.films.map((item, index) => {
       return (
-        <div key={item.title} id="movie-list-wrapper">
-          <Movie item={item} />
+        <div key={item.episode_id} id="movie-list-wrapper">
+          <Movie item={item} index={index} />
         </div>
       )
     })
@@ -27,8 +48,19 @@ class App extends Component {
         <img src={logo1} alt="star wars text"/>
           <br/>
         <img src={logo2} alt="move rater text"/>
-
-        {films}
+        <DragDropContext onDragEnd={this.onDragEnd}>
+          <Droppable droppableId="droppable-1">
+            {(provided) => (
+              <div
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+                >
+                {provided.placeholder}
+                {films}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
       </div>
     );
   }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,26 +2,38 @@ import React from 'react'
 import { useState } from 'react'
 import './movie.css'
 import { formatDate } from '../assets/functions/fns'
+import { Draggable } from 'react-beautiful-dnd'
 
 
 export default function Movie(props) {
-    let { item } = props;
+    let { item, index } = props;
     const [isShown, changeShown] = useState(false);
     let date = formatDate(item.release_date)
     return (
-        <div className="movie-wrapper">
-            <h1>{item.title}</h1>
-            {isShown ?
-            <span>
-                <span className="show-less" onClick={() => changeShown(false)}> -</span>
-                <div>
-                    Released: {date} <br/>
-                    Chronological Order: {item.episode_id} <br/>
-                </div>
-            </span> :
+        <Draggable draggableId={item.episode_id} index={index}>
+        {(provided) => (
+            <div className="movie-wrapper"
+                {...provided.draggableProps}
+                {...provided.dragHandleProps}
+                ref={provided.innerRef}
+            >
+                <h1>{index + 1}. &nbsp; </h1>
+                    <h1>{item.title}</h1>
+                    {isShown ?
+                    <span>
+                            <span className="show-less" onClick={() => changeShown(false)}> -</span>
+                                <div>
+                                    Released: {date} <br/>
+                                    Chronological Order: {item.episode_id} <br/>
+                                    Director: {item.director} <br/>
+                                    Producer: {item.producer} <br/>
+                                </div>
+                    </span> :
 
-            <span className="show-more" onClick={() => changeShown(true)}> +</span>}
+                <span className="show-more" onClick={() => changeShown(true)}> +</span>}
 
-        </div>
+            </div>
+        )}
+        </Draggable>
     )
 }
